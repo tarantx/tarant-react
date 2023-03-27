112a2d66698ec88d25bc436948ce44b68fe88ccd
kanekotic <me@kanekotic.com>
2023-03-27 19:38:37 +0200

    ALVARO | added ci cd

--- lib/react-renderer.ts
+++ lib/react-renderer.ts
@@ -1,38 +1,42 @@
 import { Actor, ActorMessage, IMaterializer } from 'tarant'
-import React from 'react';
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 const baseProps = [
-    "self",
-    "materializers",
-    "scheduled",
-    "topicSubscriptions",
-    "busy",
-    "id",
-    "partitions",
-    "system",
-    "supervisor",
-    "__internals"
+  'self',
+  'materializers',
+  'scheduled',
+  'topicSubscriptions',
+  'busy',
+  'id',
+  'partitions',
+  'system',
+  'supervisor',
+  '__internals',
 ]
 /* tslint:disable */
-export class ReactRenderer implements IMaterializer{
+export class ReactRenderer implements IMaterializer {
   root: any
-  
+
   public onInitialize(actor: Actor): void {
     //
     const localActor: any = actor
     localActor.__internals = localActor.__internals || {}
     localActor.__internals.react = Object.getOwnPropertyNames(actor)
-        .filter(prop => !baseProps.includes(prop))
-        .map((name) => {
-            return {name, prop:actor[name] }
-        })
-    const domContainer = document.getElementById(localActor.id);
-    this.root = ReactDOM.createRoot(domContainer);
-    this.root.render(localActor.render(localActor.__internals.react.reduce((acc,value) => {
-        acc[value.name]=value.prop
-        return acc
-    },{})))
+      .filter((prop) => !baseProps.includes(prop))
+      .map((name) => {
+        return { name, prop: actor[name] }
+      })
+    const domContainer = document.getElementById(localActor.id)
+    this.root = ReactDOM.createRoot(domContainer)
+    this.root.render(
+      localActor.render(
+        localActor.__internals.react.reduce((acc, value) => {
+          acc[value.name] = value.prop
+          return acc
+        }, {}),
+      ),
+    )
   }
 
   public onBeforeMessage(actor: Actor, message: ActorMessage): void {
@@ -42,10 +46,14 @@ export class ReactRenderer implements IMaterializer{
   public onAfterMessage(actor: Actor, message: ActorMessage): void {
     //
     const localActor: any = actor
-    this.root.render(localActor.render(localActor.__internals.react.reduce((acc,value) => {
-        acc[value.name]=value.prop
-        return acc
-    },{})))
+    this.root.render(
+      localActor.render(
+        localActor.__internals.react.reduce((acc, value) => {
+          acc[value.name] = value.prop
+          return acc
+        }, {}),
+      ),
+    )
   }
 
   public onError(actor: Actor, message: ActorMessage, error: any): void {
--- test/react-renderer.spec.ts
+++ test/react-renderer.spec.ts
@@ -1,45 +1,7 @@
-const mockVue = {}
-jest.mock('vue', () => ({ __esModule: true, createApp: jest.fn(() => mockVue) }))
-import { VueRenderer } from '../lib/vue-renderer'
-import { createApp } from 'vue'
+import { ReactRenderer } from '../lib/react-renderer'
 import { faker } from '@faker-js/faker'
 import { Actor } from 'tarant'
 
-describe('VueRenderer', () => {
-  it('should bind with vue in initialize', () => {
-    const expectedData = { [faker.datatype.uuid()]: faker.datatype.uuid() }
-    const expectedTemplate = faker.datatype.uuid()
-
-    class TestActorWithMethod extends Actor {
-      constructor() {
-        super(faker.datatype.uuid())
-      }
-
-      public method(): void {
-        return
-      }
-
-      data() {
-        return expectedData
-      }
-
-      template() {
-        return expectedTemplate
-      }
-    }
-
-    const renderer = new VueRenderer()
-    const fakeActorInstance: any = new TestActorWithMethod()
-    fakeActorInstance.self = fakeActorInstance
-
-    renderer.onInitialize(fakeActorInstance)
-    expect(createApp).toBeCalledWith({
-      data: expect.anything(),
-      el: `#${fakeActorInstance.id}`,
-      render: expect.anything(),
-      methods: {
-        method: expect.anything(),
-      },
-    })
-  })
+describe('ReactRenderer', () => {
+  it('should bind with react in initialize', () => {})
 })
